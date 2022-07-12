1ee1a819467e2348affcb235db6269ea7e39e46b
seongyoon25 <seongyoonk25@gmail.com>
2022-07-12 10:13:26 +0900

    fitting added

--- impedancecircuit/models/circuit.py
+++ impedancecircuit/models/circuit.py
@@ -11,34 +11,44 @@ class Circuit:
         circuit = circuit.replace(' ', '')
         self.circuit = circuit
 
-        circuit_func = build_circuit(self.circuit)
+        circuit_func, parameters = build_circuit(self.circuit)
         self.circuit_func = circuit_func
+        self.parameters_init = np.array(parameters)
 
     def fit(self, frequency, impedance):
-        parameters = fit_circuit(self.circuit_func, frequency, impedance)
-        self.parameters = parameters
-        return self
+        parameters = fit_circuit(self.circuit_func, self.parameters_init,
+                                 frequency, impedance)
+        self.parameters = np.array(parameters)
+
+    def predict(self, frequency):
+        impedance = self.circuit_func(frequency, *self.parameters)
+        return impedance
 
 
 def build_circuit(circuit):
     # circuit = 'l-r-(r,cpe)-(r-cpe,cpe)'
     # circuit = 'l-(cpe,(cpe,r)-r)-r-(cpe,r)'
 
+    # initial parameters will be determined after circuit is identified.
+    parameters = np.ones(10)
+
     def circuit_func(frequency, *parameters):
         parameters = np.array(parameters).tolist()
         # Temporarily fixed circuit
-        z = L(parameters[0], frequency) + \
-            R(parameters[1], frequency) + \
-            p([R(parameters[2], frequency),
-               CPE(parameters[3], frequency)]) + \
-            p([R(parameters[4], frequency) + CPE(parameters[5], frequency),
-               CPE(parameters[6], frequency)])
+        z = L([parameters[0]], frequency) + \
+            R([parameters[1]], frequency) + \
+            p([R([parameters[2]], frequency),
+               CPE([parameters[3], parameters[4]], frequency)]) + \
+            p([R([parameters[5]], frequency) +
+               CPE([parameters[6], parameters[7]], frequency),
+               CPE([parameters[8], parameters[9]], frequency)])
         z_real = np.real(z)
         z_imag = np.imag(z)
-        return np.stack([z_real, z_imag], 1)
-    return circuit_func
+        return np.concatenate([z_real, z_imag])
+    return circuit_func, parameters
 
 
-def fit_circuit(circuit_func, frequency, impedance):
-    parameters, _ = curve_fit(circuit_func, frequency, impedance)
+def fit_circuit(circuit_func, parameters, frequency, impedance):
+    parameters, _ = curve_fit(circuit_func, frequency, impedance,
+                              p0=parameters)
     return parameters
--- impedancecircuit/tests/test_circuit.py
+++ impedancecircuit/tests/test_circuit.py
@@ -1,21 +1,38 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+import sys
+sys.path.insert(0, '/home/ubuntu/seongyoon/PI/Impedance')
 from impedancecircuit.models.circuit import Circuit
 
 
 cellname = '25C01'
-datapath = './data/Cavendish/'
+datapath = '../examples/data/Cavendish/'
 filename = f'{datapath}EIS_state_V_{cellname}.txt'
 
 data = pd.read_csv(filename, delimiter='\t')
 data.columns = data.columns.str.strip()
 
 cycle = 1
-data_real = data[data['cycle number'] == cycle]['Re(Z)/Ohm']
-data_imag = data[data['cycle number'] == cycle]['-Im(Z)/Ohm']
-data_freq = data[data['cycle number'] == cycle]['freq/Hz']
+data_real = data[data['cycle number'] == cycle]['Re(Z)/Ohm'].values
+data_imag = data[data['cycle number'] == cycle]['-Im(Z)/Ohm'].values
+data_freq = data[data['cycle number'] == cycle]['freq/Hz'].values
+
+impedance = np.concatenate([data_real, data_imag])
 
 
 def test_circuit():
     circuit_str = 'l-r-(r,cpe)-(r-cpe,cpe)'
     circuit = Circuit(circuit_str)
+
+    circuit.fit(data_freq, impedance)
+    print(circuit.parameters)
+    impedance_pred = circuit.predict(data_freq)
+    print(impedance_pred)
+    plt.plot(data_real, data_imag, '.')
+    plt.plot(impedance_pred[:len(impedance_pred)//2], impedance_pred[len(impedance_pred)//2:], '-')
+    plt.show()
+
+
+test_circuit()
